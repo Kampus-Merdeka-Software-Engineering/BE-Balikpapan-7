0a3d4d929e813b93fb7f3adf5cccea13d73d985b
tun156 <145894451+tun156@users.noreply.github.com>
2023-10-09 06:28:37 +0700

    update category

--- controller/categoryController.js
+++ controller/categoryController.js
@@ -1,9 +1,9 @@
 const categoryService = require("../service/categoryService");
 
-// get all articles in blog
+// get all Category in blog
 async function getCategory(req, res) {
   try {
-    const articles = await categoryService.getCategory();
+    const Category = await categoryService.getCategory();
     res.status(200).json({
       message: "Successfully fetched all articles in blog",
       data: Category,
@@ -14,17 +14,17 @@ async function getCategory(req, res) {
   }
 }
 
-// Get an article by ID
+// Get an Category by ID
 async function getCategoryById(req, res) {
   const { CategoryId } = req.params;
   try {
     const Category = await categoryService.getCategoryById(CategoryId);
     if (!Category) {
-      return res.status(404).json({ error: "article not found" });
+      return res.status(404).json({ error: "Category not found" });
     }
     res.status(200).json({
-      message: "Successfully fetched article",
-      data: article,
+      message: "Successfully fetched Category",
+      data: Category,
     });
   } catch (error) {
     console.error(error);
