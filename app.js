73bb72ab7381e0a19406561d5871143c66c21e00
tun156 <145894451+tun156@users.noreply.github.com>
2023-10-09 19:20:44 +0700

    update

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const PORT = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: true }));
 
 const { teamRoutes } = require("./route/teamRoutes");
 const { categoryRoutes } = require("./route/categoryRoutes");
